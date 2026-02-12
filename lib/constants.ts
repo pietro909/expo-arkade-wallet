966d44291f88a0c4dbc4358e0764b8c68176e154
Pietro Grandi <dev@pietro.uno>
2026-02-12 15:26:35 +0100

    Use envvars

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,18 +1,14 @@
-import Constants from 'expo-constants'
-
 export const arknoteHRP = 'arknote'
 export const defaultFee = 0
 export const defaultPassword = 'noah'
 export const minSatsToNudge = 100_000
 export const maxPercentage = 10
 
-const devServer = 'http://192.168.0.115:7070'
-const testServer = 'https://arkade.computer'
-const mainServer = 'https://arkade.computer'
+const devServer = process.env.EXPO_PUBLIC_ARK_DEV_SERVER ?? 'http://localhost:7070'
+const testServer = process.env.EXPO_PUBLIC_ARK_TEST_SERVER ?? 'https://arkade.computer'
+const mainServer = process.env.EXPO_PUBLIC_ARK_MAIN_SERVER ?? 'https://arkade.computer'
 
 export const defaultArkServer = (): string => {
-  const envServer = Constants.expoConfig?.extra?.arkServer
-  if (envServer) return envServer
   return devServer
 }
 
